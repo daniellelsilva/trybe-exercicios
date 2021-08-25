d2aca7d23f6aa708dad9eeaf463295e9499c6ed0
Danielle Silva <danielle.luisasilva@gmail.com>
2021-08-25 20:15:51 -0300

    adiciona exercicio 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-1.js
@@ -0,0 +1,18 @@
+const a = 7;
+const b = 8;
+
+// Exercicio 1-a (adição)
+console.log(a + b);
+
+// EXercicio 1-b (subtração)
+console.log(a - b);
+
+// Exercicio 1-c (multiplicação)
+console.log(a * b);
+
+// Exercicio 1-d (divisão)
+console.log(a/b);
+
+// Exercicio 1-e (módulo)
+console.log(a % b);
+
