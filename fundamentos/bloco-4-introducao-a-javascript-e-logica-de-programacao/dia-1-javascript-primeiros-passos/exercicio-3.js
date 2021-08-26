da9ac93b5337e7abdfb70f73f75356f07429ea9b
Danielle Silva <danielle.luisasilva@gmail.com>
2021-08-25 21:46:25 -0300

    adiciona exercicio 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-3.js
@@ -0,0 +1,11 @@
+const a = 27;
+const b = 33;
+const c = 8;
+
+if (a > b && a > c) {
+    console.log("a é o maior número");
+} else if (b > a && b > c) {
+    console.log("b é o maior dos números");
+} else {
+    console.log("c é o maior dos números");
+}
