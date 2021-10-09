de6b61b9b0c938cc693347ffb2e509b1a1b63c2d
Danielle Silva <danielle.luisasilva@gmail.com>
2021-10-09 20:22:37 -0300

    adiciona exercicio 8

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios/exercicio8.js
@@ -0,0 +1,5 @@
+const greet = (user = 'pessoa', greeting = 'Hi') => (`${greeting} ${user}!`)
+
+console.log(greet('John')) // 'Hi John'
+console.log(greet('John', 'Good morning')) // 'Good morning John'
+console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
