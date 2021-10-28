b34a6877e679b139792000de23b52cf499fdbb94
Danielle Silva <danielle.luisasilva@gmail.com>
2021-10-27 23:04:16 -0300

    adiciona exercicio 2

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-2/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './Header.jsx';
+import Content from './Content.jsx';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Content/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
