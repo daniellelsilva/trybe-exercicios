a2ddfe240afa3525a9f6070ca528e672ed04203f
Danielle Silva <danielle.luisasilva@gmail.com>
2021-11-03 20:01:35 -0300

    adiciona exercicio de fixacao 8

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
@@ -21,25 +21,30 @@ class App extends React.Component {
     this.setState((estadoAnterior, _props) => ({
       cliquesBotao1: estadoAnterior.cliquesBotao1 + 1
     }))
-}
+  }
 
-handleClick2 = () => {
-  this.setState((estadoAnterior, _props) => ({
-    cliquesBotao2: estadoAnterior.cliquesBotao2 + 1
-  }))
-}
+  handleClick2 = () => {
+    this.setState((estadoAnterior, _props) => ({
+      cliquesBotao2: estadoAnterior.cliquesBotao2 + 1
+    }))
+  }
 
-handleClick3 = () => {
-  this.setState((estadoAnterior, _props) => ({
-    cliquesBotao3: estadoAnterior.cliquesBotao3 + 1
-  }))
-}
+  handleClick3 = () => {
+    this.setState((estadoAnterior, _props) => ({
+      cliquesBotao3: estadoAnterior.cliquesBotao3 + 1
+    }))
+  }
+
+  changeBtnColor(number) {
+    return number % 2 === 0 ? 'green' : 'white';
+  }
+
   render() {
     return (
     <div>
-      <button onClick={ this.handleClick1 }>Botão 1 | Count = {this.state.cliquesBotao1}</button>
-      <button onClick={ this.handleClick2 }>Botão 2 | Count = {this.state.cliquesBotao2}</button>
-      <button onClick={ this.handleClick3 }>Botão 3 | Count = {this.state.cliquesBotao3}</button>
+      <button onClick={ this.handleClick1 } style={{ backgroundColor: this.changeBtnColor(this.state.cliquesBotao1) }}>Botão 1 | Count = {this.state.cliquesBotao1}</button>
+      <button onClick={ this.handleClick2 } style={{ backgroundColor: this.changeBtnColor(this.state.cliquesBotao2) }}>Botão 2 | Count = {this.state.cliquesBotao2}</button>
+      <button onClick={ this.handleClick3 } style={{ backgroundColor: this.changeBtnColor(this.state.cliquesBotao3) }}>Botão 3 | Count = {this.state.cliquesBotao3}</button>
     </div>
     )}
 }
